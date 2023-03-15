641a20df2bc2c46f79bec94fcd2b9353c8e38d80
harelfogel <fogell06@gmail.com>
2023-03-15 10:15:28 +0200

    adding call fucntionality

--- src/components/Building.js
+++ src/components/Building.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Floor } from './Floor';
 import { FloorLabel } from './FloorLabel';
 import { CallButton } from './CallButton';
@@ -7,6 +7,70 @@ import '../styles/styles.css';
 
 export const Building = () => {
   const { floors, elevators } = buildingData;
+  const initialElevatorState = Array(elevators).fill({ currentFloor: 0, status: 'idle' });
+  const [elevatorState, setElevatorState] = useState(initialElevatorState);
+  const [requestQueue, setRequestQueue] = useState([]);
+
+
+  const handleElevatorCall = (requestedFloor) => {
+    console.log(`Elevator requested at floor ${requestedFloor + 1}`);
+
+    const closestElevator = findClosestElevator(requestedFloor);
+    if (closestElevator != null) {
+      console.log(`Elevator ${closestElevator + 1} is moving to floor ${requestedFloor + 1}`);
+      moveElevator(closestElevator, requestedFloor);
+    } else {
+      console.log('No availble elevators at this time');
+      // Find the closest elevator and send it to the requested floor
+      // Add the request to the queue
+      setRequestQueue((prevQueue) =>[...prevQueue,requestedFloor])
+    }
+  };
+
+  const findClosestElevator = (requestedFloor) => {
+    let closestElevator = null;
+    let minDistance = Infinity;
+
+    elevatorState.forEach((elevator, index) => {
+      const { currentFloor, status } = elevator;
+
+      if (status === 'idle' || (status === 'moving' && currentFloor <= requestedFloor)) {
+        const distance = Math.abs(requestedFloor - currentFloor);
+
+        if (distance < minDistance) {
+          closestElevator = index;
+          minDistance = distance;
+        }
+      }
+
+    });
+    return closestElevator;
+  };
+
+  const moveElevator = (elevatorIndex, targetFloor) => {
+    const newState = [...elevatorState];
+    newState[elevatorIndex] = { currentFloor: targetFloor, status: 'idle' };
+    setElevatorState(newState);
+    console.log(`Elevator ${elevatorIndex + 1} moved to floor ${targetFloor + 1}`);
+  
+    // Schedule to check the queue after a delay
+    setTimeout(() => {
+      // Check if there are pending requests in the queue
+      if (requestQueue.length > 0) {
+        // Get the next request from the queue
+        const nextRequest = requestQueue[0];
+  
+        // Remove the processed request from the queue
+        const updatedQueue = requestQueue.slice(1);
+        setRequestQueue(updatedQueue);
+  
+        // Move the elevator to the next request
+        moveElevator(elevatorIndex, nextRequest);
+      }
+    }, 1000); // Adjust the delay as needed
+  };
+
+
 
   return (
     <div className="building-wrapper">
@@ -22,7 +86,7 @@ export const Building = () => {
       </div>
       <div className="call-buttons">
         {floors.map((_, floor) => (
-          <CallButton key={floor} floor={floor} />
+          <CallButton key={floor} floor={floor} onElevatorCall={handleElevatorCall} />
         ))}
       </div>
     </div>
--- src/components/CallButton.js
+++ src/components/CallButton.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/styles.css';
 
-export const CallButton = ({ floor }) => {
+export const CallButton = ({ floor,onElevatorCall  }) => {
+  const [isWaiting, setIsWaiting] = useState(false);
+
   const handleClick = () => {
-    // Implement your logic for handling button clicks here
+    setIsWaiting(true);
+    onElevatorCall(floor);
     console.log(`Call elevator to floor ${floor + 1}`);
   };
 
+  const buttonClass = isWaiting ? 'call-button waiting' : 'call-button';
+
   return (
-    <button onClick={handleClick} className="call-button">
-      Call
+    <button onClick={handleClick} className={buttonClass}>
+      {isWaiting ? 'Waiting' : 'Call'}
     </button>
   );
 };
